862ff4f24a6e07d95695ff425ba57260e7694c35
lxqddd <xiangqian_liu@163.com>
2025-01-22 19:16:36 +0800

    config seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,20 @@
+import path from 'node:path'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  site: {
+    url: '',
+    name: '图片格式转换工具',
+  },
+  seo: {
+    meta: {
+      description: '图片格式转换工具',
+    },
+  },
+  srcDir: path.resolve(__dirname, 'src'),
+  alias: {
+    '@': path.resolve(__dirname, 'src'),
+  },
+  modules: ['@unocss/nuxt', '@nuxtjs/robots', '@nuxtjs/seo'],
 })
